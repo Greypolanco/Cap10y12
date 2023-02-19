44a46e225d58e6afb362ece993243e53b6e017f0
Greilyn Polanco <greilyntejada03@gmail.com>
2023-02-18 22:16:26 -0400

    Mi primer commit

---
+++ Inventario.cs
@@ -0,0 +1,14 @@
+namespace Cap10_12
+{
+
+    public class Inventario
+    {
+        public string? productName { get; set; }
+        public int quantity { get; set; }
+        public float price { get; set; }
+
+        
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Cap10_12
+{
+    public class Ejercicios
+    {
+
+        public static void Main(String[] args)
+        {
+
+        }
+    }
+}
