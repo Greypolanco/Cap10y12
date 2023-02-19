4cc168579d9b75a31287a0fe833dd4489ac98797
Greilyn Polanco <greilyntejada03@gmail.com>
2023-02-19 17:18:49 -0400

    Ejercicio 1 cap 10

--- Inventario.cs
+++ Inventario.cs
@@ -1,11 +1,19 @@
 namespace Cap10_12
 {
 
-    public class Inventario
+    public class Inventory
     {
         public string? productName { get; set; }
+        public int idproduct { get; set; }
         public int quantity { get; set; }
-        public float price { get; set; }
+        public double price { get; set; }
+        public Inventory(string productName, int quantity, double price, int idproduct)
+        {
+            this.productName = productName;
+            this.quantity = quantity;
+            this.price = price;
+            this.idproduct = idproduct;
+        }
 
         
         
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Cap10_12
 
         public static void Main(String[] args)
         {
-
+            Inventory product1 = new Inventory("lapiz",30,15.00, 1);
+            System.Console.WriteLine(product1);
         }
     }
 }
