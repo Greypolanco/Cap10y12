3dc2240eeb0497383720ff63372361e603611de6
Greilyn Polanco <greilyntejada03@gmail.com>
2023-02-19 22:46:48 -0400

    Ejercicio 4 y 5 capi10

--- Polygon.cs
+++ Polygon.cs
@@ -2,7 +2,7 @@ namespace Cap10_12
 {
     public class Polygon
     {
-        public int side { get; set; }
+        public int side {get; set;}
         public int side_quantity { get; set; }
         public int perimeter { get; set; }
         public double area { get; set; }
@@ -18,6 +18,19 @@ namespace Cap10_12
             this.height = height;
             perimeter = side * side_quantity;
             area = (perimeter * height)/2;
+        }
+
+        public void SetSide(int side)
+        {   
+            if(this.side > 0)
+                this.side = side;
+            else 
+                System.Console.WriteLine("Tiene que ser mayor a cero");
+        }
+
+        public override string ToString()
+        {
+            return $" los lado: {side}, cantidad: {side_quantity}";
         }
     }
 }
